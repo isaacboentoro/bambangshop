40b450a6eadcfdfb0d2758943c2758f2d32d639b
isaacboentoro <isaac.jesse@ui.ac.id>
2025-03-25 11:32:33 +0700

    Implement delete function in Subscriber repository.

--- src/model/repository/subscriber.rs
+++ src/model/repository/subscriber.rs
@@ -3,26 +3,26 @@ use lazy_static::lazy_static;
 use crate::model::subscriber::Subscriber;
 
 // Singleton of Database
-lazy_static!{
+lazy_static! {
     static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::new();
 }
+
 pub struct SubscriberRepository;
 
 impl SubscriberRepository {
     pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
-        let subscriber_value = subscriber.clone();
-        if SUBSCRIBERS.get(product_type).is_none(){
+        let subscribe_value = subscriber.clone();
+        if SUBSCRIBERS.get(product_type).is_none() {
             SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
-
         };
 
         SUBSCRIBERS.get(product_type).unwrap()
-            .insert(subscriber_value.url.clone(), subscriber_value);
-        return subscriber;
+            .insert(subscribe_value.url.clone(), subscribe_value);
+        return subscriber
     }
 
     pub fn list_all(product_type: &str) -> Vec<Subscriber> {
-        if SUBSCRIBERS.get(product_type).is_none(){
+        if SUBSCRIBERS.get(product_type).is_none() {
             SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
         };
 
@@ -30,4 +30,15 @@ impl SubscriberRepository {
             .map(|f| f.value().clone()).collect();
     }
 
+    pub fn delete(product_type: &str, url: &str) -> Option<Subscriber> {
+        if SUBSCRIBERS.get(product_type).is_none() {
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        }
+        let result = SUBSCRIBERS.get(product_type).unwrap()
+            .remove(url);
+        if !result.is_none() {
+            return Some(result.unwrap().1);
+        }
+        return None;
+    }
 }
